c7b5f9b3010de2a7a51a94b5d2846db7aeb801ea
Andreas Krings-Stern <github@ankri.de>
2020-10-27 20:59:24 +0100

    re-added getstaticpaths again

--- pages/details/[slug].js
+++ pages/details/[slug].js
@@ -2,28 +2,25 @@ export default function Details({ slug }) {
   return <div>Details: {slug}</div>;
 }
 
-//
-// It also fails when un-commenting getStaticPaths
-//
-// export const getStaticPaths = () => {
-//   return {
-//     paths: [
-//       {
-//         params: {
-//           slug: "slug-1",
-//         },
-//         locale: "de",
-//       },
-//       {
-//         params: {
-//           slug: "slug-1",
-//           locale: "en",
-//         },
-//       },
-//     ],
-//     fallback: true,
-//   };
-// };
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      {
+        params: {
+          slug: "slug-1",
+        },
+        locale: "de",
+      },
+      {
+        params: {
+          slug: "slug-1",
+          locale: "en",
+        },
+      },
+    ],
+    fallback: true,
+  };
+};
 
 export const getStaticProps = async ({ params }) => {
   return {
