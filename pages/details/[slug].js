4fd5317483156fcea35d603e4206bdb237bbb476
Andreas Krings-Stern <github@ankri.de>
2020-10-27 20:54:58 +0100

    shorter example

--- pages/details/[slug].js
+++ pages/details/[slug].js
@@ -2,25 +2,28 @@ export default function Details({ slug }) {
   return <div>Details: {slug}</div>;
 }
 
-export const getStaticPaths = () => {
-  return {
-    paths: [
-      {
-        params: {
-          slug: "slug-1",
-        },
-        locale: "de",
-      },
-      {
-        params: {
-          slug: "slug-1",
-          locale: "en",
-        },
-      },
-    ],
-    fallback: true,
-  };
-};
+//
+// It also fails when un-commenting getStaticPaths
+//
+// export const getStaticPaths = () => {
+//   return {
+//     paths: [
+//       {
+//         params: {
+//           slug: "slug-1",
+//         },
+//         locale: "de",
+//       },
+//       {
+//         params: {
+//           slug: "slug-1",
+//           locale: "en",
+//         },
+//       },
+//     ],
+//     fallback: true,
+//   };
+// };
 
 export const getStaticProps = async ({ params }) => {
   return {
