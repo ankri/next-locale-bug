e125a9c305d089af979d7e5f657cd1c444cd98ae
Andreas Krings-Stern <github@ankri.de>
2020-10-27 20:48:33 +0100

    added reproduction example

---
+++ pages/details/[slug].js
@@ -0,0 +1,31 @@
+export default function Details({ slug }) {
+  return <div>Details: {slug}</div>;
+}
+
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      {
+        params: {
+          slug: "slug-1",
+        },
+        locale: "de",
+      },
+      {
+        params: {
+          slug: "slug-1",
+          locale: "en",
+        },
+      },
+    ],
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  return {
+    props: {
+      slug: params.slug,
+    },
+  };
+};
