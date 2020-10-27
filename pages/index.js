88c6c900a4d5f67d6f331ca6c953fe6a9873dc5b
Andreas Krings-Stern <github@ankri.de>
2020-10-27 21:04:58 +0100

    added example using Router

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,44 @@
 import NextLink from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const Router = useRouter();
+
   return (
     <div>
       <p>Home</p>
       <ul>
         <li>
           <NextLink href="/details/slug-1" passHref locale="de">
-            <a>/de/details</a>
+            <a>/de/details/slug-1</a>
           </NextLink>
         </li>
         <li>
           <NextLink href="/details/slug-1" passHref locale="en">
-            <a>/en/details</a>
+            <a>/en/details/slug-1</a>
           </NextLink>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              Router.push("/details/slug-1", undefined, {
+                locale: "de",
+              });
+            }}
+          >
+            /de/details/slug-1
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              Router.push("/details/slug-1", undefined, {
+                locale: "en",
+              });
+            }}
+          >
+            /en/details/slug-1
+          </button>
         </li>
       </ul>
     </div>
